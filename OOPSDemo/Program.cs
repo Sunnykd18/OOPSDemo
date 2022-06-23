f543a45c57a22b9802dd3b506baba5fa5d18f61e
ADA-475 <ADA-475@ADA-475>
2022-06-23 22:11:39 +0530

    Add project files.

---
+++ OOPSDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare and initialize on same line
+            Rectangle rectangle = new Rectangle();
+
+            // declare
+            Rectangle rectangle1;
+            //initialize 
+            rectangle1 = new Rectangle();
+
+            rectangle.Length = int.Parse(Console.ReadLine());
+           // rectangle.Width = int.Parse(Console.ReadLine());
+            Console.WriteLine("Area of rectangle is " + rectangle.Area());
+            Console.ReadLine();
+
+           
+        }
+    }
+}
---
+++ OOPSDemo/Rectangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSDemo
+{
+    internal class Rectangle
+    {
+        // fields - attribute
+        // methods - behaviour
+
+        private int length, width;
+
+        public int Length
+        {
+            get { return length; }
+            set { length = value; width = 7; } // parameter
+        }
+        public int Width
+        { 
+            get { return width; }
+            set { width = value; } // parameter
+        }
+     
+        public int Area()
+        {
+            return length * width;
+        }
+        public Rectangle()
+        {
+            Length = 1;
+            Width = 1;
+        }
+    }
+}
